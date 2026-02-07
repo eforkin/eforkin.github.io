612770678234a900c7392bc4beebdff2f2ea0a11
Erik Forkin <eforkin@umich.edu>
2026-02-07 00:27:00 -0600

    convert to be apps hub

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
 $(document).ready(function(){
-      $('.parallax').parallax();
-    });
-
-$(document).on('click', 'a', function(event){
-
-    var href = $.attr(this, 'href');
-
-    if (href.charAt(0) == "#") {
-      event.preventDefault();
-
-      $('html, body').animate({
-          scrollTop: $( $.attr(this, 'href') ).offset().top
-      }, 500);
-    }
+    $('.parallax').parallax();
 });
